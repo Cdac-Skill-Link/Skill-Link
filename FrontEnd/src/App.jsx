4f5e4cf7c0a61f2ebfe2418721a12696ab1a6ef6
magdumrushikesh <rushikesh4525@gmail.com>
2025-02-05 23:46:17 +0530

    initial frontend

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,42 @@
+// 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import './index.css';
+
+// import * as serviceWorker from './serviceWorker';
+// import { Contact } from './components/contact';
+// // import UserLogin from './components/Login';
+// // import App5 from './Login page/App5';
+// import Login from './components/UserLogin';
+// import Registration from './components/userRegistration';
+// import ServiceLogin from './components/serviceProviderLogin';
+// import { ServiceList } from './components/Loginservices/ServiceList';
+// import JsonData from "./data/data.json";
+// import LandingPage from "./components/Landingpage";
+// import ServiceForm from './components/Loginservices/ServiceForm';
+// import ServiceRegistration from "./components/serviceProviderRegistration"
+// import ServiceTable from './components/ServiceProvider/ServiceList';
+
+// import { ServiceproviderPage } from './components/ServiceProvider/ServiceProviderPage';
+// import BookingTable from './components/Loginservices/BookingList';
+// import ConfirmTable from './components/ServiceProvider/ConfirmList';
+// import ForgotPassword from './components/Loginservices/forgotPassword';
+// import FeedbackForm from './components/Loginservices/Rating';
+// import ServiceForgotPassword from './components/ServiceProvider/ServiceForgotPassword';
+
+
+const App = () => {
+
+
+
+  return (
+    <div>
+   
+      <h1>Skill Link</h1>
+    </div>
+  );
+};
+
+export default App;
