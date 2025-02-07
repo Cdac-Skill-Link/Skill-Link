20cab711647769d5dd3f451fdb7bc1d79646e099
magdumrushikesh <rushikesh4525@gmail.com>
2025-02-07 22:56:32 +0530

    resolved import issue

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,31 +1,31 @@
-// 
+
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import './index.css';
+import './index.css';
 
-// import * as serviceWorker from './serviceWorker';
-// import { Contact } from './components/contact';
+import * as serviceWorker from './serviceWorker';
+import { Contact } from './components/contact';
 // // import UserLogin from './components/Login';
 // // import App5 from './Login page/App5';
 
 import Registration from './components/userRegistration';
-// import ServiceLogin from './components/serviceProviderLogin';
-// import { ServiceList } from './components/Loginservices/ServiceList';
-// import JsonData from "./data/data.json";
+import ServiceLogin from './components/serviceProviderLogin';
+import { ServiceList } from './components/Loginservices/ServiceList';
+import JsonData from "./data/data.json";
 import LandingPage from "./components/Landingpage";
 import Login from './components/UserLogin';
-// import ServiceForm from './components/Loginservices/ServiceForm';
-// import ServiceRegistration from "./components/serviceProviderRegistration"
-// import ServiceTable from './components/ServiceProvider/ServiceList';
+import ServiceForm from './components/Loginservices/ServiceForm';
+import ServiceRegistration from "./components/serviceProviderRegistration"
+import ServiceTable from './components/ServiceProvider/ServiceList';
 
-// import { ServiceproviderPage } from './components/ServiceProvider/ServiceProviderPage';
-// import BookingTable from './components/Loginservices/BookingList';
-// import ConfirmTable from './components/ServiceProvider/ConfirmList';
-// import ForgotPassword from './components/Loginservices/forgotPassword';
-// import FeedbackForm from './components/Loginservices/Rating';
-// import ServiceForgotPassword from './components/ServiceProvider/ServiceForgotPassword';
+import { ServiceproviderPage } from './components/ServiceProvider/ServiceProviderPage';
+import BookingTable from './components/Loginservices/BookingList';
+import ConfirmTable from './components/ServiceProvider/ConfirmList';
+import ForgotPassword from './components/Loginservices/forgotPassword';
+import FeedbackForm from './components/Loginservices/Rating';
+import ServiceForgotPassword from './components/ServiceProvider/ServiceForgotPassword';
 
 
 const App = () => {
