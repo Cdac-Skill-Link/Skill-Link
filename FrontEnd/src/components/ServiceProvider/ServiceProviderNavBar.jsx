59d560d1007c5a55b8079bd649c7481bf0b3fbc3
VaibhavWarekar <vaibhavwarekar2001@gmail.com>
2025-02-08 19:25:06 +0530

    confirmList added Underline Effect

--- FrontEnd/src/components/ServiceProvider/ServiceProviderNavBar.jsx
+++ FrontEnd/src/components/ServiceProvider/ServiceProviderNavBar.jsx
@@ -1,65 +1,65 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 export const ServiceProviderNavbar = () => {
-    // const username=sessionStorage["username"];
-    const namefirst=sessionStorage["namefirst"];
-    // const userId=sessionStorage["userId"]
-    
-    const navigate=useNavigate()
+    const namefirst = sessionStorage["namefirst"];
+    const navigate = useNavigate();
+
     const handleLogout = () => {
         sessionStorage.clear();
-        // Perform logout logic (clear authentication)
-        //  setIsLoggedIn(false);
         navigate("/");
-      };
-  return (
-    <>
-    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
-      <div className="container">
-        <div className="navbar-header">
-          <button
-            type="button"
-            className="navbar-toggle collapsed"
-            data-toggle="collapse"
-            data-target="#bs-example-navbar-collapse-1"
-          >
-            {" "}
-            <span className="sr-only">Toggle navigation</span>{" "}
-            <span className="icon-bar"></span>{" "}
-            <span className="icon-bar"></span>{" "}
-            <span className="icon-bar"></span>{" "}
-          </button>
-          <a className="navbar-brand page-scroll" href="#page-top">
-          👤{namefirst}
-          </a>{" "}
-        </div>
-       
-        <div
-          className="collapse navbar-collapse"
-          id="bs-example-navbar-collapse-1"
-        >
-          <ul className="nav navbar-nav navbar-right">
-          
-          <li>
-              <a href="confirmList" className="page-scroll">
-                Confirm List
-              </a>
-            </li>
-            <li>
-              <a href=""  onClick={handleLogout}>
-                LogOut
-              </a>
-             
-            </li>
-           
-           
-         
-          </ul>
-        </div>
-      </div>
-    </nav>
-  
-    </>
-  );
+    };
+
+    return (
+      <>
+        <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+            <div className="container">
+                <div className="navbar-header">
+                    <button
+                        type="button"
+                        className="navbar-toggle collapsed"
+                        data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1"
+                    >
+                        <span className="sr-only">Toggle navigation</span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </button>
+                    <a className="navbar-brand page-scroll" href="#page-top">
+                        👤{namefirst}
+                    </a>
+                </div>
+
+                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul className="nav navbar-nav navbar-right">
+                        <li>
+                            <NavLink to="/data" className={({ isActive }) => isActive ? "active-link" : ""}>
+                                User Request
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/confirmList" className={({ isActive }) => isActive ? "active-link" : ""}>
+                                Confirm List
+                            </NavLink>
+                        </li>
+                        <li>
+                            <a href="#" onClick={handleLogout}>LogOut</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            
+        </nav>
+        <style>
+                {`
+                    .active-link {
+    border-bottom: 3px solid #6182FE; /* Underline effect */
+                        font-weight: bold;
+                        color: #6182FE;
+}`}</style>
+    
+               
+        </>
+    );
 };
